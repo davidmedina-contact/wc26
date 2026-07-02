02ffe9be8f70f8b3d406d11a0c7cd7512a435649
David Medina <david@medina.contact>
2026-07-01 23:30:05 -0700

    Stamp extra-time scorer release

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 var CACHE = 'wc26-v20';
-var BUILD_TS = '2026-07-02T05:30:30.139Z'; // auto-updated by npm run stamp-sw
+var BUILD_TS = '2026-07-02T06:29:19.481Z'; // auto-updated by npm run stamp-sw
 
 // Only precache assets that rarely change
 var PRECACHE = [
